94c67200bc4ee4ada22fd1e6334d3ff555b1ee53
jonbrundagejr <jbrundagejr@gmail.com>
2021-05-24 20:31:16 -0400

    form showing

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,9 +5,23 @@ class CompaniesController < ApplicationController
   def show
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.create(company_params)
+    redirect_to company_path(@company)
+  end
+
   private
 
   def find_company
     @company = Company.find(params[:id])
   end
+
+  def company_params
+    params.require(:company).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :buildings, only: [:index, :show, :edit, :update]
-  resources :companies, only: [:show]
+  resources :companies, only: [:show, :new, :create]
   resources :employees
   resources :offices
 end
